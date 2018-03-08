4f03be3da6566a5a6df521d0c79bef79f78a6b4a
David <davidchavez.4c@gmail.com>
2018-03-08 09:51:19 -0700

    Cambios pequeños

---
+++ Efecto/Delay.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.Wave;
+
+namespace Efecto
+{
+    class Delay : ISampleProvider
+    {
+        private ISampleProvider fuente;
+
+        int offsetTiempoMS;
+
+        public Delay(ISampleProvider fuente)
+        {
+            this.fuente = fuente;
+            offsetTiempoMS = 1000;
+        }
+
+        public WaveFormat WaveFormat
+        {
+            get
+            {
+                return fuente.WaveFormat;
+            }
+        }
+
+        //Offset es el numero de muestras leidas
+        public int Read(float[] buffer, int offset, int count)
+        {
+            var read = fuente.Read(buffer, offset, count);
+
+            for(int i = 0; i < read; i++)
+            {
+                
+            }
+
+            return read;
+        }
+    }
+}
